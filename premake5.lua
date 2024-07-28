53d70e6454e1ec4215e13dbf814b149f7cef42dc
KaleniG <kalenig.01@gmail.com>
2024-07-28 20:48:43 +0200

    GLFW-Vulkan oriented development

--- premake5.lua
+++ premake5.lua
@@ -19,7 +19,11 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+    "backends/imgui_impl_glfw.h",
+    "backends/imgui_impl_glfw.cpp",
+    "backends/imgui_impl_vulkan.h",
+    "backends/imgui_impl_vulkan.cpp"
 	}
 
 	filter "system:windows"
